1abb35b13c38cd868dfed3d888520cf8265242ce
ddr4869 <bikdu@naver.com>
2024-08-06 21:04:04 +0900

    apply Asia/Seoul TimeZone

--- src/main/java/dino/junction/JunctionApplication.java
+++ src/main/java/dino/junction/JunctionApplication.java
@@ -6,10 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 //@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @SpringBootApplication()
 @Import(LogAop.class)
 public class JunctionApplication {
+
+	@PostConstruct
+	void setTImeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JunctionApplication.class, args);
